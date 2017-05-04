69726cfe86f4414935d939f4050ddfbb1430e16e
othesius-exe <othesius.exe@gmail.com>
2017-05-03 20:54:28 -0400

    basic layout and all classes added

---
+++ app/src/main/java/com/example/caleb/habittracker/EditorActivity.java
@@ -0,0 +1,10 @@
+package com.example.caleb.habittracker;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Code for the Editor page
+ */
+
+public class EditorActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/example/caleb/habittracker/HabitActivity.java
+++ app/src/main/java/com/example/caleb/habittracker/HabitActivity.java
@@ -3,7 +3,12 @@ package com.example.caleb.habittracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.caleb.habittracker.data.HabitDbHelper;
+
 public class HabitActivity extends AppCompatActivity {
+
+    // Instantiate a HabitDbHelper to use throughout the activity
+    private HabitDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
